78449268a4a15eedfba6892d7ebcb0c294ace631
Ako2003 <akifmursalov@gmail.com>
2023-07-30 17:00:43 +0400

    Computer 3D

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -9,11 +9,24 @@ function Computers(){
   
   return (
     <mesh>
-      <hemisphereLight intensity={0.15}
-      groundColor="black" />
+      <hemisphereLight 
+      intensity={0.15}
+      groundColor="black"
+      />
       <pointLight intensity={1} />
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
       <primitive
         object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   )
@@ -32,8 +45,8 @@ const ComputersCanvas = () => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2} 
           minPolarAngle={Math.PI / 2}
-        />
-        <Computers />
+          />
+          <Computers />
         </Suspense>
 
         <Preload all />
@@ -43,4 +56,4 @@ const ComputersCanvas = () => {
   )
 }
 
-export default Computers;
+export default ComputersCanvas;
